c8f02a76fb7e7f69f9948180bfeb2a072441f87b
patchwork-body <personal.gugfug@gmail.com>
2023-07-12 15:51:55 +0200

    feat: change pino pretty to console.log

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { createApp } from "./app";
-import { logger } from "./logger";
 
 const port = process.env.PORT || 5001;
 const server = createApp();
 
 server.listen(Number(port), '0.0.0.0', () => {
-  logger.info(`Server listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`)
 });
